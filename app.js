fa23bd2b05a3747a1195edaec83f9a06a9599fe4
Victor050904 <victorortizr09@gmail.com>
2023-07-02 23:02:56 -0500

    version_1

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').addEventListener('click', function() {
+    const section = document.getElementById('AcercaDe');
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+  
